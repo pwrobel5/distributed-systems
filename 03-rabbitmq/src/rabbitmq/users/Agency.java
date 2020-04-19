0ec15330decb3a90bb04b56a2285c6a286d6fc75
Piotr Wróbel <pwrobel5@gmail.com>
2020-04-19 19:00:27 +0200

    RabbitMQ initial commit

---
+++ 03-rabbitmq/src/rabbitmq/users/Agency.java
@@ -0,0 +1,9 @@
+package rabbitmq.users;
+
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Agency {
+    public static void main(String[] args) {
+        System.out.println("Agency");
+    }
+}
---
+++ 03-rabbitmq/src/rabbitmq/users/Carrier.java
@@ -0,0 +1,7 @@
+package rabbitmq.users;
+
+public class Carrier {
+    public static void main(String[] args) {
+        System.out.println("Carrier");
+    }
+}
